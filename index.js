7ed4c015cabd086afcb4abe1d9973d5a722bed0f
Łukasz Piłatowski <lpilatowski@teonite.com>
2018-01-10 16:00:08 +0100

    move to ES5 and add bin command

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const program = require('commander');
-const path = require('path');
-const fs = require('fs');
-const po2json = require('po2json');
+#! /usr/bin/env node
+var program = require('commander');
+var path = require('path');
+var fs = require('fs');
+var po2json = require('po2json');
 
 function convert_file(source, target) {
     if(path.extname(source) !== '.po') {
@@ -14,9 +15,9 @@ function convert_file(source, target) {
     }
     console.log('Converting! ', source, target);
 
-    const source_obj = fs.readFileSync(path.resolve(source));
-    const parsed_source = po2json.parse(source_obj);
-    for (const key in parsed_source) {
+    var source_obj = fs.readFileSync(path.resolve(source));
+    var parsed_source = po2json.parse(source_obj);
+    for (var key in parsed_source) {
         if (parsed_source.hasOwnProperty(key)) {
             parsed_source[key] = parsed_source[key][1];
         }
@@ -26,18 +27,18 @@ function convert_file(source, target) {
 
 program
     .version('1.0.0')
-    .usage('[oprions] <source> <target>')
+    .usage('[options] <source> <target>')
     .arguments('<source> <target>')
     .option('-r, --recursive', 'Recursively convert all files in directory')
     .parse(process.argv);
 
-const NO_COMMAND_SPECIFIED = program.args.length === 0;
+var NO_COMMAND_SPECIFIED = program.args.length === 0;
 
 if(NO_COMMAND_SPECIFIED) {
    program.help();
 }
 
-const source = program.args[0], target = program.args[1];
+var source = program.args[0], target = program.args[1];
 
 if(!program.recursive) {
     convert_file(source, target);
@@ -45,20 +46,22 @@ if(!program.recursive) {
     if (!fs.existsSync(target)) {
         fs.mkdirSync(target);
     }
-    fs.readdir(source, (err, filenames) => {
+    fs.readdir(source, function(err, filenames) {
         if(!filenames) {
             console.error('No files in source directory!');
             process.exit(1);
         }
-        const files = filenames.filter(file => path.extname(file) === '.po');
+        const files = filenames.filter(function(file) {
+            return path.extname(file) === '.po';
+        });
 
         if(files.length === 0) {
             console.error('No .json files to convert in target directory!');
             process.exit(1);
         }
 
-        files.forEach(file => {
-            const target_file = path.basename(file, path.extname(file)) + '.json';
+        files.forEach(function(file) {
+            var target_file = path.basename(file, path.extname(file)) + '.json';
             convert_file(path.join(source, file), path.join(target, target_file));
         })
     })
